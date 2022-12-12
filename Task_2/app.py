467ab43bf2d6571df3cf8aadb5fd15c9c69cf2b5
sharmaak <51890180+akshat-sangri@users.noreply.github.com>
2022-12-12 22:43:04 +0530

    Updated commit

--- Task_2/app.py
+++ Task_2/app.py
@@ -7,13 +7,17 @@ app = Flask(__name__)
 # Load the translation dataset from a file
 df = pd.read_excel("data/cleaned_translation-data.xlsx")
 
+@app.route('/api/v1/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
 @app.route('/api/v1/translations', methods=['GET'])
 def get_translations():
     # Select 10 random translation pairs from the dataset
-    10_sentence = df.sample(10).to_dict('examples')
-    status: HTTPStatus
-    source, target = 10_sentence
-    return jsonify({"source": source, "target": target}, status)
+    ten_sentence = df.sample(10).to_dict('examples')
+
+    source, target = ten_sentence
+    return jsonify({"source": source, "target": target})
+
 
 if __name__ == '__main__':
     app.run()
